40ee7dadca51b6af2abbec8d805fb573bdf8653a
cs686 <cs686@126.com>
2015-11-03 00:07:11 +0800

    new2

---
+++ testgit/testgit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testgit
+//
+//  Created by 顺苹亓 on 15/11/3.
+//  Copyright © 2015年 顺苹亓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testgit/testgit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testgit
+//
+//  Created by 顺苹亓 on 15/11/3.
+//  Copyright © 2015年 顺苹亓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
